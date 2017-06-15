a77e56a7e494d2d9ab9c792a764f7206d517abf7
SIC\chayanan <chayanan@sic.co.th>
2017-06-15 17:52:10 +0700

    - patch OCB - parsing Data - getrequest

--- DjangoW/app/ocb.py
+++ DjangoW/app/ocb.py
@@ -8,27 +8,36 @@ def ocb_encrypt(key,nonce,associated,plaintext):
     associated = binascii.unhexlify(associated)
     plaintext = binascii.unhexlify(plaintext)
     #print("key:   ",binascii.hexlify(key).upper())
-    #print("associated: ",binascii.hexlify(associated).upper())
     #print("nonce: ",binascii.hexlify(nonce).upper())
+    #print("associated: ",binascii.hexlify(associated).upper())
     #print("plaintext:",binascii.hexlify(plaintext).upper())
 
     cipher = AES.new(key, AES.MODE_OCB, nonce)
+    cipher.update(associated)
     ciphertext, mac = cipher.encrypt_and_digest(plaintext)
 
-    #print("cipher:   ",binascii.hexlify(ciphertext).upper())
-    #print("mac tag:  ",binascii.hexlify(mac).upper())
+    #print("cipher:   %s|%s" % (binascii.hexlify(ciphertext).upper(),binascii.hexlify(mac).upper()))
+    #print("expect:   81017F8203F081277152FADE694A0A00",)
 
-    return binascii.hexlify(mac).upper(),binascii.hexlify(mac).upper()
+    #return binascii.hexlify(ciphertext).upper(),binascii.hexlify(mac).upper()
+    return ciphertext,mac
 
-def ocb_decrypt(key,ciphertext,mac):
+def ocb_decrypt(key,nonce,associated,ciphertext,mac):
+    key = binascii.unhexlify(key)
+    nonce = binascii.unhexlify(nonce)
+    associated = binascii.unhexlify(associated)
+
     cipher = AES.new(key, AES.MODE_OCB, nonce=nonce)
     try:
+        cipher.update(associated)
         plaintext = cipher.decrypt_and_verify(ciphertext, mac)
     except ValueError:
         print("Invalid message")
     else:
         print("decrypted:",binascii.hexlify(plaintext).upper())
 
+    return binascii.hexlify(plaintext).upper()
 
 if __name__ == "__main__":
-    ocb_encrypt('000102030405060708090A0B0C0D0E0F','BBAA99887766554433221101','0001020304050607','0001020304050607')
+    ci,tg = ocb_encrypt('000102030405060708090A0B0C0D0E0F','BBAA99887766554433221102','0001020304050607','0001020304050607')
+    ocb_decrypt('000102030405060708090A0B0C0D0E0F','BBAA99887766554433221102','0001020304050607',ci,tg)
--- DjangoW/app/views.py
+++ DjangoW/app/views.py
@@ -11,13 +11,32 @@ from app.ocb import *
 
 def home(request):
     """Renders the home page."""
+    key = '000102030405060708090A0B0C0D0E0F'
+    nonce = 'BBAA99887766554433221103'
+    associated = ''
+    #if request.GET.get('U'):
+    #    message = 'You submitted: %r' % request.GET['U']
+    #else:
+    #    message = 'You submitted nothing'
+    #UID = ''+request.GET.get('U').upper().encode()
+    #TF = binascii.hexlify(request.GET.get('TF').upper())
+    #TS = ''+request.GET.get('TS')
+    #RLC = request.GET.get('RLC')
+    PT = "%s%s%s%s" % (request.GET.get('U'),binascii.hexlify(request.GET.get('TF').upper()),request.GET.get('TS'),request.GET.get('RLC'))
+    [cipher,tag] = ocb_encrypt(key,nonce,'',PT)
     assert isinstance(request, HttpRequest)
     return render(
         request,
         'app/index.html',
         {
+            #/?U=E0391122334455&TF=AA&TS=0000019B&RLC=D7B9EEA9C9FD689333AD097A9BD79C12B67A0FA6130BBC7890F12AEADF01206CB78F9DDA10A56933AE08F12588
             'namee':random.choice(['Dogs', 'Cats', 'Bears']),
-            'ocb':ocb_encrypt('000102030405060708090A0B0C0D0E0F','BBAA99887766554433221101','0001020304050607','0001020304050607'),
+            #'getu':request.GET.get('U'),
+            #'gettf':binascii.hexlify(request.GET.get('TF').upper()),
+            #'getts':request.GET.get('TS'),
+            #'getrlc':request.GET.get('RLC'),
+            'ocb': [binascii.hexlify(cipher).upper(),binascii.hexlify(tag).upper()],
+            'decry': ocb_decrypt(key,nonce,associated,cipher,tag),
             'title':'Home Page',
             'year':datetime.now().year,
         }
